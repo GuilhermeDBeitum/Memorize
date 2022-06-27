14e9411513846db283d919120ff4fd5de1b1fc0e
douglasbeitum <xpointersite@outlook.com>
2022-06-26 23:47:57 -0300

    Initial Commit

---
+++ Xpointer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Xpointer
+//
+//  Created by Guilherme Douglas Beitum on 26/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Xpointer/XpointerApp.swift
@@ -0,0 +1,17 @@
+//
+//  XpointerApp.swift
+//  Xpointer
+//
+//  Created by Guilherme Douglas Beitum on 26/06/22.
+//
+
+import SwiftUI
+
+@main
+struct XpointerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
